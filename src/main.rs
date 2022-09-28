6accde67e54b595bddff2d26d7a350bbc9ce01b9
skoky <skokys@gmail.com>
2022-09-28 20:38:34 +0200

    adding speech sample

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use windows::Devices::WiFi::{ WiFiAdapter, WiFiAvailableNetwork, WiFiNetworkReport};
-use windows::Networking::Connectivity::NetworkAdapter;
+use windows::Devices::WiFi::{WiFiAdapter, WiFiAvailableNetwork, WiFiNetworkReport};
+use windows::Foundation::{AsyncStatus, IAsyncOperation};
+use windows::Media::SpeechRecognition::{SpeechRecognitionCompilationResult, SpeechRecognitionResult, SpeechRecognizer};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    futures::executor::block_on(report_wifi())
+    let _ = futures::executor::block_on(report_wifi());
+    futures::executor::block_on(speech())
 }
 
-async fn report_wifi() -> Result<(),Box<dyn std::error::Error>>{
+async fn speech() -> Result<(), Box<dyn std::error::Error>> {
+    let speech: SpeechRecognizer = SpeechRecognizer::new()?;
+    let c: IAsyncOperation<SpeechRecognitionCompilationResult> = speech.CompileConstraintsAsync()?;
+
+    // TODO better await
+    loop {
+        let status: AsyncStatus = c.Status()?;
+        if status == AsyncStatus::Completed {
+            break;
+        }
+    }
+    println!("Listening...");
+
+    let result: IAsyncOperation<SpeechRecognitionResult> = speech.RecognizeAsync()?;
+
+    // TODO better await
+    loop {
+        let status: AsyncStatus = result.Status()?;
+        if status == AsyncStatus::Completed {
+            break;
+        }
+    }
+    let x: SpeechRecognitionResult = result.get()?;
+    println!(">>> {}", x.Text()?);
+    Ok(())
+}
+
+async fn report_wifi() -> Result<(), Box<dyn std::error::Error>> {
     let nm = WiFiAdapter::RequestAccessAsync()?;
     println!("{:?}", nm.get()?);
     let adapter = WiFiAdapter::FindAllAdaptersAsync()?;
@@ -16,7 +45,7 @@ async fn report_wifi() -> Result<(),Box<dyn std::error::Error>>{
             let n: WiFiAvailableNetwork = network;
             println!("Network: {:?} {}kHz Signal: {} Timestamp: {:?}", n.Ssid()?,
                      n.ChannelCenterFrequencyInKilohertz()?, n.SignalBars()?,
-            n.Uptime()?);
+                     n.Uptime()?);
         }
     }
     Ok(())

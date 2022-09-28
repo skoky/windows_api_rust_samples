829ffb653309056674ff04d6678c2372ea1fc2d5
skoky <skokys@gmail.com>
2022-09-28 18:43:03 +0200

    remove unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
-use windows::Devices::WiFi::{WiFiAccessStatus, WiFiAdapter, WiFiAvailableNetwork, WiFiNetworkReport};
-use windows::Foundation::Collections::IVectorView;
-use windows::Foundation::IAsyncOperation;
+use windows::Devices::WiFi::{ WiFiAdapter, WiFiAvailableNetwork, WiFiNetworkReport};
 use windows::Networking::Connectivity::NetworkAdapter;
 
-fn main() {
-    futures::executor::block_on(main_async())
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    futures::executor::block_on(report_wifi())
 }
 
-fn report() {
-    println!("Report Wifi")
-}
-
-async fn main_async() {
-    let nm = WiFiAdapter::RequestAccessAsync().unwrap();
-    println!("{:?}", nm.get().unwrap());
-    let adapter = WiFiAdapter::FindAllAdaptersAsync().unwrap();
-    for a in adapter.get().unwrap() {
+async fn report_wifi() -> Result<(),Box<dyn std::error::Error>>{
+    let nm = WiFiAdapter::RequestAccessAsync()?;
+    println!("{:?}", nm.get()?);
+    let adapter = WiFiAdapter::FindAllAdaptersAsync()?;
+    for a in adapter.get()? {
         let adapter: WiFiAdapter = a;
-
-        let na: NetworkAdapter = adapter.NetworkAdapter().unwrap();
-        println!("{:?}", na);
-        // let report: WiFiNetworkReport = adapter.AvailableNetworksChanged(report).unwrap();
-        let report: WiFiNetworkReport = adapter.NetworkReport().unwrap();
-        for network in report.AvailableNetworks().unwrap() {
+        let report: WiFiNetworkReport = adapter.NetworkReport()?;
+        for network in report.AvailableNetworks()? {
             let n: WiFiAvailableNetwork = network;
-            println!("Network: {:?} {}kHz Signal: {} Timestamp: {:?}", n.Ssid().unwrap(),
-                     n.ChannelCenterFrequencyInKilohertz().unwrap(), n.SignalBars().unwrap(),
-            n.Uptime().unwrap());
+            println!("Network: {:?} {}kHz Signal: {} Timestamp: {:?}", n.Ssid()?,
+                     n.ChannelCenterFrequencyInKilohertz()?, n.SignalBars()?,
+            n.Uptime()?);
         }
     }
+    Ok(())
 }

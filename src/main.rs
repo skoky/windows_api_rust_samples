a75ad367e00cacab5784ecfcd2b97580c4b69ba0
skoky <skokys@gmail.com>
2022-09-29 18:04:25 +0200

    adding confidence

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use windows::core::HSTRING;
 use windows::Devices::WiFi::{WiFiAdapter, WiFiAvailableNetwork, WiFiNetworkReport};
 use windows::Foundation::{AsyncStatus, IAsyncOperation};
 use windows::Globalization::Language;
-use windows::Media::SpeechRecognition::{SpeechRecognitionCompilationResult, SpeechRecognitionResult, SpeechRecognizer, SpeechRecognizerTimeouts};
+use windows::Media::SpeechRecognition::{SpeechRecognitionCompilationResult, SpeechRecognitionConfidence, SpeechRecognitionResult, SpeechRecognizer, SpeechRecognizerTimeouts};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = futures::executor::block_on(report_wifi());
@@ -19,6 +19,7 @@ async fn speech() -> Result<(), Box<dyn std::error::Error>> {
     println!("Languages: {:?} / {:?}", language.DisplayName()?, language2.DisplayName()?);
     let c: IAsyncOperation<SpeechRecognitionCompilationResult> = speech.CompileConstraintsAsync()?;
     // speech.ContinuousRecognitionSession()
+
     let timeouts: SpeechRecognizerTimeouts = speech.Timeouts()?;
     println!("Timeouts silence: {:?} bable: {:?}", timeouts.EndSilenceTimeout()?, timeouts.BabbleTimeout()?);
 
@@ -45,7 +46,14 @@ async fn speech() -> Result<(), Box<dyn std::error::Error>> {
         }
         let x: SpeechRecognitionResult = result.get()?;
         let sentence: HSTRING = x.Text()?;
-        println!(">>> {}", sentence);
+        let confidence: SpeechRecognitionConfidence = x.Confidence()?;
+        let confidence_text = match confidence {
+            SpeechRecognitionConfidence::High => "High",
+            SpeechRecognitionConfidence::Medium => "Medium",
+            SpeechRecognitionConfidence::Low => "Low",
+            _ => "?"
+        };
+        println!(">>> ({}): {}", confidence_text, sentence);
         if sentence.to_string_lossy() == "exit" {
             exit(0);
         }

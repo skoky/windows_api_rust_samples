57f48449ac748747339ef7655db3f3cf76bed90d
skoky <skokys@gmail.com>
2022-10-08 20:19:45 +0200

    geolocation sample

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use std::process::exit;
 
 use windows::core::HSTRING;
+use windows::Devices::Geolocation::{BasicGeoposition, CivicAddress, Geocoordinate, GeolocationAccessStatus, Geolocator, Geoposition, PositionStatus};
 use windows::Devices::WiFi::{WiFiAdapter, WiFiAvailableNetwork, WiFiNetworkReport};
 use windows::Foundation::{AsyncStatus, IAsyncOperation};
 use windows::Globalization::Language;
 use windows::Media::SpeechRecognition::{SpeechRecognitionCompilationResult, SpeechRecognitionConfidence, SpeechRecognitionResult, SpeechRecognitionResultStatus, SpeechRecognizer, SpeechRecognizerTimeouts};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = futures::executor::block_on(print_geolocation());
     let _ = futures::executor::block_on(report_wifi());
     futures::executor::block_on(speech())
+}
+
+async fn print_geolocation() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Position start");
+    let locator = Geolocator::new()?;
+    let status: GeolocationAccessStatus = Geolocator::RequestAccessAsync()?.await?;
+    println!("Access {:?}", status);
+    if status == GeolocationAccessStatus::Allowed {
+
+        let position: Geoposition = locator.GetGeopositionAsync()?.await?;
+        let acc = locator.DesiredAccuracyInMeters()?;
+
+        let ls: PositionStatus = locator.LocationStatus()?;
+
+        println!("{:?}", ls);
+
+        let address: CivicAddress = position.CivicAddress()?;
+        let coordinates: Geocoordinate = position.Coordinate()?;
+        println!("Position: {:?}, {:?} / accuracy meters: {:?}", address.City()?, coordinates, acc);
+    } else {
+        println!("Geo Status: {:?}/Denied", status);
+    }
+    Ok(())
 }
 
 async fn speech() -> Result<(), Box<dyn std::error::Error>> {
@@ -45,7 +70,6 @@ async fn speech() -> Result<(), Box<dyn std::error::Error>> {
             exit(0);
         }
     }
-
 }
 
 async fn report_wifi() -> Result<(), Box<dyn std::error::Error>> {
